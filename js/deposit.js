d8a9d619c69fc21fed862da051a4f7322c8269bd
tareque-bin-habib <shad.tbh@gmail.com>
2022-09-27 22:37:46 +0600

    deposit balance updated

--- js/deposit.js
+++ js/deposit.js
@@ -1,4 +1,9 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = getInput('deposit-field');
-    console.log(depositField);
+    const depositTotal = getAmount('deposit-total');
+    const newDepositTotal = depositField + depositTotal;
+    setValue('deposit-total', newDepositTotal);
+    const balanceTotal = getAmount('balance-total');
+    const newBalanceTotal = balanceTotal + depositField;
+    setValue('balance-total', newBalanceTotal);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,22 @@
+// Get the input Field
 function getInput(inputField) {
     const getInputField = document.getElementById(inputField);
     const getInputFieldValueString = getInputField.value;
     const getInputFieldValue = parseFloat(getInputFieldValueString);
     getInputField.value = '';
     return getInputFieldValue;
+}
+
+// Get the Balance
+function getAmount(innerValue) {
+    const getTotalAmount = document.getElementById(innerValue);
+    const getTotalAmountValueString = getTotalAmount.innerText;
+    const getTotalAmountValue = parseFloat(getTotalAmountValueString);
+    return getTotalAmountValue;
+}
+
+// set the value
+function setValue(oldValue, newValue) {
+    const newElement = document.getElementById(oldValue);
+    newElement.innerText = newValue;
 }
